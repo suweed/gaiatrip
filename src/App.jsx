b0cd5c770aeec6e64283699d2d5a08b7fc00d704
jesus cardozo <gsuskr2o@gmail.com>
2023-10-23 20:27:08 -0600

    starter project react three fiber drei

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+
+function App() {
+
+  return (
+    <>
+      <Canvas camera={{
+        position: [0, 0, 5],
+        fov: 30,
+      }}>
+        <color attach="background" args={["#ececec"]} />
+        <Experience />
+      </Canvas>
+    </>
+  )
+}
+
+export default App
