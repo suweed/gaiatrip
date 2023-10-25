a03de88075dda1a2ec24c8ddea10f296998007e8
jesus cardozo <gsuskr2o@gmail.com>
2023-10-24 20:19:03 -0600

    scroll principal linea path van

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,96 @@
-import { OrbitControls } from "@react-three/drei";
+import { Line, OrbitControls, PerspectiveCamera, useScroll } from "@react-three/drei";
+import { Background } from "./Background";
+import { Van } from "./Van";
+import { Eames } from "./Eames";
+import * as THREE from "three";
+import { useMemo, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+const LINE_NB_POINTS = 2000;
 
 export const Experience = () => {
+
+  const curve = useMemo(() => {
+    return new THREE.CatmullRomCurve3(
+      [
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, -10),
+        new THREE.Vector3(-2, 0, -20),
+        new THREE.Vector3(-3, 0, -30),
+        new THREE.Vector3(0, 0, -40),
+        new THREE.Vector3(5, 0, -50),
+        new THREE.Vector3(7, 0, -60),
+        new THREE.Vector3(5, 0, -70),
+        new THREE.Vector3(0, 0, -80),
+        new THREE.Vector3(0, 0, -90),
+        new THREE.Vector3(0, 0, -100),
+      ],
+      false,
+      "catmullrom",
+      0.5
+    )
+  }, []);
+
+  const linePoints = useMemo(() => {
+    return curve.getPoints(LINE_NB_POINTS);
+  }, [curve])
+
+  const shape = useMemo(() => {
+    const shape = new THREE.Shape();
+    shape.moveTo(0, -0.2);
+    shape.lineTo(0, 0.2);
+
+    return shape;
+  }, [curve])
+
+  const cameraGroup = useRef();
+  const scroll = useScroll();
+
+  useFrame((_state, delta) => {
+    const curPointIndex = Math.min(
+      Math.round(scroll.offset * linePoints.length),
+      linePoints.length - 1
+    )
+    const curPoint = linePoints[curPointIndex];
+
+    cameraGroup.current.position.lerp(curPoint, delta * 24);
+  })
+  
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group ref={cameraGroup}>
+        <Background />
+        <PerspectiveCamera position={[0, 0, 5]} fov={30} makeDefault />
+        <Van rotation-y={Math.PI / 0.99} position={[0, -1, -2]} scale={[0.6, 0.6, 0.6]} />
+      </group>
+
+      {/* LINE */}
+      <group position-y={-1}>
+        {/* <Line
+          points={linePoints}
+          color={"white"}
+          opacity={0.7}
+          transparent
+          lineWidth={16}
+        /> */}
+        <mesh>
+          <extrudeGeometry
+            args={[
+              shape,
+              {
+                steps: LINE_NB_POINTS,
+                bevelEnabled: false,
+                extrudePath: curve
+              }
+            ]}
+          />
+          <meshStandardMaterial color={"white"} opacity={0.7} transparent />
+        </mesh>
+      </group>
+
+      <Eames opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 1, -3]} />
+      <Eames opacity={0.5} scale={[0.2, 0.3, 0.4]} position={[1.5, -0.5, -2]} />
+      <Eames opacity={0.7} scale={[0.3, 0.3, 0.4]} position={[2, -0.2, -2]} rotation-y={Math.PI / 9} />
     </>
   );
 };
